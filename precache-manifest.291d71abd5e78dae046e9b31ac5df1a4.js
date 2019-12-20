986344a9578d80f83eb178090a31fdba38201469
Farrah Carter <carterfarraha@gmail.com>
2019-12-20 15:44:21 -0500

    Updates

---
+++ precache-manifest.291d71abd5e78dae046e9b31ac5df1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd9d17e2e57308941a1879dd84917e6",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "36ad93dc0bdd097bb3e1",
+    "url": "/clickygame/static/css/main.36b6977b.chunk.css"
+  },
+  {
+    "revision": "1b1a735827d0d2699794",
+    "url": "/clickygame/static/js/2.951c9641.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.951c9641.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36ad93dc0bdd097bb3e1",
+    "url": "/clickygame/static/js/main.d6b6d4d4.chunk.js"
+  },
+  {
+    "revision": "f950880ffe804219cdd9",
+    "url": "/clickygame/static/js/runtime-main.76c8915d.js"
+  }
+]);
