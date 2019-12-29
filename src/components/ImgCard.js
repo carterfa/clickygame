6098e2226091b5964af87ae46614e87fe3a23415
Farrah Carter <carterfarraha@gmail.com>
2019-12-28 21:52:45 -0500

    Adjusted mobile optimization

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -4,7 +4,7 @@ function ImgCard(props) {
     const url = "assets/click_images/" + props.src;
     return (
 
-        <div className="col-4">
+        <div className="col-12 col-md-6 col-lg-4">
             <img className="clickImage" onClick={props.onClick} src={url} alt={props.src} val={props.val} />
         </div>
 
